d3dbd4ae857be9ea11147fcc11fe26f5bb2f5459
sorrawit <mr.flukesorrawit@gmail.com>
2025-11-03 11:24:37 +0700

    save

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -1,5 +1,13 @@
-import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+import 'package:dart_application_1/dart_application_1.dart'
+    as dart_application_1;
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_application_1.calculate()}!');
+//ข้อ1
+void main() {
+  print("Welcome to Dart!");
+}
+
+//ข้อ2
+void main2() {
+  var planet = "Earth";
+  print(planet);
 }
